b809b1064a8245a8da81f476d396688fe8408f94
And-Rasta <avangaea@gmail.com>
2019-02-12 13:31:35 -0600

    commit for heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: false }));
 app.use(bodyParser.json());
 
-// Set up view engine
+// Set up view engine / handlebars
 app.engine('handlebars', exphbs({defaultLayout: 'main' }))
 app.set('view engine', 'handlebars');
 
@@ -22,10 +22,25 @@ app.set('view engine', 'handlebars');
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost:27017/news-scraper");
 
+// Require routes
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Application started");
+});
+
+// Connect to Mongo DB
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function () {
+    console.log(
+        "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+        PORT,
+        PORT
+    );
 });
